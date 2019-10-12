0a9fa7035aa9be262cfdbe9d330063b416882d4c
SteliosPhanartzis <stelios.phan@gmail.com>
2019-10-12 01:20:15 -0400

    Updated routes in main router

--- api/app.js
+++ api/app.js
@@ -4,7 +4,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var brandsRouter = require('./routes/brands');
+var productsRouter = require('./routes/products');
+var rewardsRouter = require('./routes/rewards');
 
 var app = express();
 
@@ -15,6 +18,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/login', usersRouter);
+app.use('/login', loginRouter);
+app.use('/brands', brandsRouter);
+app.use('/products', productsRouter);
+app.use('/rewards', rewardsRouter);
 
 module.exports = app;
