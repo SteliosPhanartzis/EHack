fb145d5078fc1497904c228b844df23e4a691315
Wadgma Masab <wadgmamasab@gmail.com>
2019-10-12 08:03:03 -0400

    Sixth Commit

--- app/src/main/java/com/app/elchackathon/HomePage.java
+++ app/src/main/java/com/app/elchackathon/HomePage.java
@@ -3,12 +3,40 @@ package com.app.elchackathon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 
 public class HomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        WebView htmlWebView = (WebView)findViewById(R.id.webView);
+        htmlWebView.setWebViewClient(new CustomWebViewClient());
+        WebSettings webSetting = htmlWebView.getSettings();
+        webSetting.setJavaScriptEnabled(true);
+        webSetting.setDisplayZoomControls(true);
+        webSetting.setAllowContentAccess(true);
+        webSetting.setAppCacheEnabled(true);
+        webSetting.setDomStorageEnabled(true);
+        webSetting.setUseWideViewPort(true);
+        htmlWebView.loadUrl("https://www.adweek.com/brand-marketing/estee-lauders-origins-brand-asks-consumers-recycle-105597/");
+
+
+
+
+
+    }
+    private class CustomWebViewClient extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
---
+++ app/src/main/java/com/app/elchackathon/StatPage.java
@@ -0,0 +1,15 @@
+package com.app.elchackathon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stat_page);
+
+    }
+}
