9d577d4c14c9c5f42c0fde0c41699b3afd048ec5
SteliosPhanartzis <stelios.phan@gmail.com>
2019-10-12 03:21:20 -0400

    Finished brands

---
+++ api/routes/brands.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var mysql = require('mysql2');
+var dotenv = require('dotenv').config()
+var router = express.Router();
+/* MySQL Set Up */
+const connection = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_SCHEMA
+});
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	connection.query('SELECT name FROM brands', (err, rows) => {
+		if (err) throw err;
+		res.json(rows);
+	});
+});
+
+module.exports = router;
